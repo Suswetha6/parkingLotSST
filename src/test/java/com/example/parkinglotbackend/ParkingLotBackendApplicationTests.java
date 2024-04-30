27c9c202b4b873306e30f6f32c93d3bf14b748a3
Suswetha <suswetha.23bcs10166@ms.sst.com>
2024-05-01 00:01:08 +0530

    commit

---
+++ src/test/java/com/example/parkinglotbackend/ParkingLotBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.parkinglotbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingLotBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
